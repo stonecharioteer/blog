c648e5cbced77a6134d6e3101a14d4b2acaa5ea9
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-09-09 12:26:14 +0530

    add wagtailmarkdown and make related migrations

--- stonecharioteer/home/models.py
+++ stonecharioteer/home/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel
 
+from wagtailmarkdown.edit_handlers import MarkdownPanel
+from wagtailmarkdown.fields import MarkdownField
 
 class HomePage(Page):
-    pass
+    body = MarkdownField()
+    content_panels = Page.content_panels + [
+        # FieldPanel('body', classname="full"),
+        MarkdownPanel("body"),
+    ]
