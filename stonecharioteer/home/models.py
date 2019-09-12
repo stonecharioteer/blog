6dfe2eecf305fd8a14b382f777c4aef0d242bc13
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-09-12 17:34:08 +0530

    add blog

--- stonecharioteer/home/models.py
+++ stonecharioteer/home/models.py
@@ -4,12 +4,13 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
-from wagtailmarkdown.edit_handlers import MarkdownPanel
-from wagtailmarkdown.fields import MarkdownField
+# from wagtailmarkdown.edit_handlers import MarkdownPanel
+# from wagtailmarkdown.fields import MarkdownField
 
 class HomePage(Page):
-    body = MarkdownField()
+    body = RichTextField(blank=True)
+
     content_panels = Page.content_panels + [
-        # FieldPanel('body', classname="full"),
-        MarkdownPanel("body"),
+        FieldPanel('body', classname="full"),
+        # MarkdownPanel("body"),
     ]
