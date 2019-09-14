26473190c1b45b7aeec54fab0ae3cb41e55eec4b
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2019-09-14 13:28:31 +0530

    enable site settings

--- stonecharioteer/stonecharioteer/settings/base.py
+++ stonecharioteer/stonecharioteer/settings/base.py
@@ -28,9 +28,11 @@ INSTALLED_APPS = [
     'search',
     'blog',
     'writing',
+    'site_settings',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
+    'wagtail.contrib.settings',
     'wagtail.embeds',
     'wagtail.sites',
     'wagtail.users',
@@ -81,6 +83,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'wagtail.contrib.settings.context_processors.settings',
             ],
         },
     },
@@ -164,4 +167,3 @@ WAGTAIL_SITE_NAME = "stonecharioteer"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'https://stonecharioteer.com'
-
