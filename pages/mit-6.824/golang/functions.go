6356e1551fcfde84f75e9fb04abca60f477357a5
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2020-07-08 22:11:34 +0530

    add programs on functions, short variable assignment and basic types

--- pages/mit-6.824/golang/functions.go
+++ pages/mit-6.824/golang/functions.go
@@ -7,10 +7,23 @@ func add(x int, y int) int {
 }
 
 func mult(x, y int) int {
-	return x*y
+	return x * y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return // this returns the named variables x, y in that order.
 }
 
 func main() {
 	fmt.Println(add(42, 12))
 	fmt.Println(mult(42, 12))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	fmt.Println(split(17))
 }
