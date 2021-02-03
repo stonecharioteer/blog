558d60c6bd427833dc934ea5be41b1fa846998ae
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2021-02-03 11:22:31 +0530

    expand hello-oso

--- hello_oso/script.py
+++ hello_oso/script.py
@@ -8,3 +8,8 @@ if oso.is_allowed("user", "can_use", "this_program"):
     print("Hello from oso")
 else:
     print("Access denied")
+
+if oso.is_allowed("user-123", "can_run", "this_program"):
+    print("Hello again, but you probably can't run this line.")
+else:
+    print("You weren't authorized by the rules!")
