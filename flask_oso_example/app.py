3988df936ee0be28ae9a387adac52cc9bcd0519f
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2021-02-07 22:11:24 +0530

    feat: add flask oso example

---
+++ flask_oso_example/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+
+import oso
+from flask_oso import FlaskOso
+
+app = Flask(__name__)
+
+base_oso = oso.Oso()
+
+oso_extension = FlaskOso(oso=base_oso)
+
+base_oso.load_str("""allow("anyone","can_visit","index");""")
+
+
+oso_extension.init_app(app)
+
+@app.route("/")
+def index_route():
+    oso_extension.authorize(actor="anyone", action="can_visit", resource="index")
+    return "hello world"
+
+
+@app.route("/unvisitable")
+def unpermissable_route():
+    oso_extension.authorize(actor="noone", action="can_visit", resource="this route")
