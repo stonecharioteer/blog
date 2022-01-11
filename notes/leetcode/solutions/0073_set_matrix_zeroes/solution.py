1f4adfb1e61cc11d573552266ae9e91ec08b23e4
Vinay Keerthi <11478411+stonecharioteer@users.noreply.github.com>
2022-01-11 16:13:06 +0530

    add solution for lc-0073

--- notes/leetcode/solutions/0073_set_matrix_zeroes/solution.py
+++ notes/leetcode/solutions/0073_set_matrix_zeroes/solution.py
@@ -4,13 +4,24 @@ Link : https://leetcode.com/problems/set-matrix-zeroes/
 Level: medium
 """
 
+from typing import List
+
 
 class Solution:
-    def __init__(self, *args, **kwargs):
-        """Instantiates the solution object"""
-        self.args = args
-        self.kwargs = kwargs
-
-    def solve(self, *args, **kwargs):
-        """This implements the main solution"""
-        raise NotImplementedError("This solution is not yet implemented.")
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # first note down where the zeroes are.
+        zeroes = []
+        for i, row in enumerate(matrix):
+            for j, cell in enumerate(row):
+                if cell == 0:
+                    zeroes.append((i, j))
+        for (i, j) in zeroes:
+            # item at i x j is 0,
+            # set row i to zero
+            # set col j to zero
+            matrix[i] = [0 for _ in matrix[i]]
+            for row, _ in enumerate(matrix):
+                matrix[row][j] = 0
